1f1f0f181e63db962d745f0f9027cb9754712c39
Peter Jang <peter.x.jang@gmail.com>
2017-11-20 18:27:23 -0600

    Add custom model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,25 @@
 class Product < ApplicationRecord
+  def is_discounted
+    price.to_f <= 2
+  end
+
+  def tax
+    price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + tax
+  end
+
+  def as_json
+    {
+      id: id,
+      name: name,
+      description: description,
+      price: price,
+      tax: tax,
+      total: total,
+      is_discounted: is_discounted,
+    }
+  end
 end
