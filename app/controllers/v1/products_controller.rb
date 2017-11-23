1bb65f39a053eae8aee1042de55f8f3c2143415c
Peter Jang <peter.x.jang@gmail.com>
2017-11-22 20:39:50 -0600

    Add options to search products by name and sort by price

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,6 +1,17 @@
 class V1::ProductsController < ApplicationController
   def index
-    products = Product.all
+    products = Product.all.order(:id => :asc)
+
+    user_search_terms = params[:search]
+    if user_search_terms
+      products = products.where("name ILIKE ?", "%" + user_search_terms + "%")
+    end
+
+    should_i_sort_by_price = params[:sort_by_price]
+    if should_i_sort_by_price
+      products = Product.all.order(:price => :asc)
+    end
+
     render json: products.as_json
   end
 
--- frontend.rb
+++ frontend.rb
@@ -3,6 +3,8 @@ require "pp"
 
 puts "Choose an option:"
 puts "[1] Show all products"
+puts "  [1.1] Show all products that match search terms"
+puts "  [1.2] Show all products sorted by price"
 puts "[2] Create a product"
 puts "[3] Show one product"
 puts "[4] Update a product"
@@ -11,6 +13,16 @@ puts "[5] Delete a product"
 input_option = gets.chomp
 if input_option == "1"
   response = Unirest.get("http://localhost:3000/v1/products")
+  products = response.body
+  pp products
+elsif input_option == "1.1"
+  print "Enter search terms: "
+  input_search_terms = gets.chomp
+  response = Unirest.get("http://localhost:3000/v1/products?search=#{input_search_terms}")
+  products = response.body
+  pp products
+elsif input_option == "1.2"
+  response = Unirest.get("http://localhost:3000/v1/products?sort_by_price=true")
   products = response.body
   pp products
 elsif input_option == "2"
