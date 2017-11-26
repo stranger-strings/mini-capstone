f9b36ea8dc483b7c7146bc57370d9672179c68a1
Peter Jang <peter.x.jang@gmail.com>
2017-11-26 11:06:56 -0600

    Add supplier model associated with product model (with updated seeds file)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121032314) do
+ActiveRecord::Schema.define(version: 20171126165538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20171121032314) do
     t.decimal "price", precision: 9, scale: 2
     t.string "image"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
